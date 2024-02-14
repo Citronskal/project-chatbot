ee780759c5fab3cb03dbe1e42e14e95524d6b087
Citronskal <yiaswift13@gmail.com>
2024-02-14 17:38:32 +0100

    v1

--- code/script.js
+++ code/script.js
@@ -1,5 +1,6 @@
 // DOM selectors (variables that point to selected DOM elements) goes here 👇
-const chat = document.getElementById('chat')
+const chat = document.getElementById("chat");
+const form = document.getElementById("chat-form");
 
 // Functions goes here 👇
 
@@ -7,7 +8,7 @@ const chat = document.getElementById('chat')
 const showMessage = (message, sender) => {
   // The if statement checks if the sender is the user and if that's the case it inserts
   // an HTML section inside the chat with the posted message from the user
-  if (sender === 'user') {
+  if (sender === "user") {
     chat.innerHTML += `
       <section class="user-msg">
         <div class="bubble user-bubble">
@@ -15,10 +16,10 @@ const showMessage = (message, sender) => {
         </div>
         <img src="assets/user.png" alt="User" />  
       </section>
-    `
+    `;
     // The else if statement checks if the sender is the bot and if that's the case it inserts
     // an HTML section inside the chat with the posted message from the bot
-  } else if (sender === 'bot') {
+  } else if (sender === "bot") {
     chat.innerHTML += `
       <section class="bot-msg">
         <img src="assets/bot.png" alt="Bot" />
@@ -26,28 +27,71 @@ const showMessage = (message, sender) => {
           <p>${message}</p>
         </div>
       </section>
-    `
+    `;
   }
 
   // This little thing makes the chat scroll to the last message when there are too many to
   // be shown in the chat box
-  chat.scrollTop = chat.scrollHeight
-}
+  chat.scrollTop = chat.scrollHeight;
+};
 
 // A function to start the conversation
 const greetUser = () => {
   // Here we call the function showMessage, that we declared earlier with the argument:
   // "Hello there, what's your name?" for message, and the argument "bot" for sender
-  showMessage("Hello there, what's your name?", 'bot')
-  // Just to check it out, change 'bot' to 'user' here 👆 and see what happens
-}
+  showMessage("Hello there, what's your name?", "bot");
+};
+
+const handleInput = (event) => {
+  event.preventDefault();
+  // Store the value in a variable so we can access it after we
+  // clear it from the input
+  const name = document.getElementById("user-input").value;
+  showMessage(name, "user");
+  document.getElementById("user-input").value = "";
+
+  // After 1 second, show the next question by invoking the next function.
+  // passing the name into it to have access to the user's name if we want
+  // to use it in the next question from the bot.
+  setTimeout(() => showFoodOptions(name), 1000);
+};
+
+const showFoodOptions = () => {
+  showMessage(
+    "What would you like to order? Please choose your tea base:",
+    "bot"
+  );
+
+  const greenTea = document.createElement("button");
+  greenTea.textContent = "Green Tea";
+  greenTea.addEventListener("click", () => handleFoodChoice("Green Tea"));
+
+  const blackTea = document.createElement("button");
+  blackTea.textContent = "Black Tea";
+  blackTea.addEventListener("click", () => handleFoodChoice("Black Tea"));
+
+  const milkTea = document.createElement("button");
+  milkTea.textContent = "Milk Tea";
+  milkTea.addEventListener("click", () => handleFoodChoice("Oolong Tea"));
+
+  chat - form.appendChild(greenTea);
+  chat - form.appendChild(blackTea);
+  chat - form.appendChild(milkTea);
+
+  // document.getElementById("user-input").style.display = showFoodOptions;
+};
+
+const handleFoodChoice = (choice) => {
+  showMessage(`You chose ${choice}.`, "user");
+};
 
 // Eventlisteners goes here 👇
+form.addEventListener("submit", handleInput);
 
 // Here we invoke the first function to get the chatbot to ask the first question when
 // the website is loaded. Normally we invoke functions like this: greeting()
 // To add a little delay to it, we can wrap it in a setTimeout (a built in JavaScript function):
 // and pass along two arguments:
-// 1.) the function we want to delay, and 2.) the delay in milliseconds 
+// 1.) the function we want to delay, and 2.) the delay in milliseconds
 // This means the greeting function will be called one second after the website is loaded.
-setTimeout(greetUser, 1000)
+setTimeout(greetUser, 1000);
